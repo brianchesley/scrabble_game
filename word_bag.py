413c41f963d64a902e5aa6bad35f76dabd76cbea
bhches <bhches@monsanto.com>
2019-03-23 08:14:12 -0500

    chaging wildcard symbol

--- word_bag.py
+++ word_bag.py
@@ -16,7 +16,7 @@ class Letter:
 
 
 class ScrabbleBag(Letter):
-    SCRABBLE_LETTERS = [' ', ' ', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'A', 'A', 'A', 'A', 'A',
+    SCRABBLE_LETTERS = ['?', '?', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'A', 'A', 'A', 'A', 'A',
                         'A', 'A', 'A', 'A', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'O', 'O', 'O', 'O', 'O', 'O',
                         'N', 'N', 'N', 'N', 'R', 'R', 'R', 'R', 'R', 'R', 'T', 'T', 'T', 'T', 'T', 'T', 'L', 'L', 'L',
                         'L', 'S', 'S', 'S', 'S', 'U', 'U', 'U', 'U', 'D', 'D', 'D', 'D', 'G', 'G', 'G', 'B', 'B', 'C',
